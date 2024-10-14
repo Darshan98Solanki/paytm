10a600d8df5c6e4a5a4b985ba5d2826fea2295b4
Darshan98Solanki <darshan98solanki@gmail.com>
2024-10-14 22:53:59 +0530

    backend change

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,15 @@ const app = express();
 const { router } = require("./routes/index");
 const cors = require('cors')
 
-app.use(cors())
+// CORS configuration
+const corsOptions = {
+    origin: 'https://paytm-axue.vercel.app', // Replace with your frontend URL
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true // Enable if sending cookies or authorization headers
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.json())
 app.use('/api/v1', router)
 
