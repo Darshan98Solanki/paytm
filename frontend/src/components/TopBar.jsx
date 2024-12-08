f9b12ce0afc57d673ad89606deca6cdd24d000ea
Darshan98Solanki <darshan98solanki@gmail.com>
2024-12-08 11:35:13 +0530

    redirection logic change

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -6,9 +6,31 @@ import { useNavigate } from "react-router-dom";
 
 export function TopBar() {
 
-  const [username,setUserName] = useState()
-  const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
+
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("token");
+
+      if (!token || token == "") {
+        navigate("../signin")
+      } else {
+        axios
+          .get("https://paytm-inky.vercel.app/api/v1/user/me", {
+            headers: {
+              authorization: localStorage.getItem("token"),
+            },
+          }).catch(error => {
+            if (!error.response.data.authorization) {
+              navigator("../signin")
+            }
+          });
+      }
+    } catch (error) { }
+  }, []);
+
+  const [username, setUserName] = useState()
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,16 +50,16 @@ export function TopBar() {
   }, []);
 
   return <div className="h-14 py-10 shadow-md shadow-cyan-400/50 flex justify-between">
-    <div className="flex font-mono text-xl lg:text-4xl font-bold flex-col justify-center h-full ml-5 lg:ml-10" onClick={()=>{
+    <div className="flex font-mono text-xl lg:text-4xl font-bold flex-col justify-center h-full ml-5 lg:ml-10" onClick={() => {
       navigate("../home")
     }}>
-        PAY-DM
+      PAY-DM
     </div>
     <div className="flex">
-        <div className="flex flex-col justify-center h-full mr-2">
-          <Dropdown username={username+" 👋"}/>
-        </div>
+      <div className="flex flex-col justify-center h-full mr-2">
+        <Dropdown username={username + " 👋"} />
+      </div>
     </div>
-    <Loader show={loading}/>
-  </div>  
+    <Loader show={loading} />
+  </div>
 }
